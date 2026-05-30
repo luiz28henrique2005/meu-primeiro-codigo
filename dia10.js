28df6588cff0fbcb2509f3c89046e91725b83ac1
luiz <luiz28henrique2005@gmail.com>
2026-05-29 22:17:23 -0300

    feat: adicionei o script motivacional dia 10

---
+++ dia10.js
@@ -0,0 +1,14 @@
+ console.log ('voltei a estudar JavaScript depois de um tempo parado, e estou gostando muito de aprender coisas novas, e espero que eu consiga aprender cada vez mais sobre essa linguagem tão incrível que é o JavaScript, e que eu possa usar esse conhecimento para criar coisas incríveis e ajudar as pessoas de alguma forma, e que eu possa me divertir muito enquanto aprendo, e que eu possa compartilhar esse conhecimento com outras pessoas também, e que eu possa me tornar um desenvolvedor cada vez melhor, e que eu possa alcançar meus objetivos e sonhos com esse conhecimento, e que eu possa ser feliz enquanto faço isso, e que eu possa aproveitar cada momento dessa jornada de aprendizado, e que eu possa ser grato por ter a oportunidade de aprender algo tão incrível como o JavaScript, e que eu possa ser paciente comigo mesmo enquanto aprendo, e que eu possa me divertir muito enquanto faço isso, e que eu possa ser persistente mesmo quando as coisas ficarem difíceis, e que eu possa me lembrar sempre do motivo pelo qual comecei a aprender JavaScript em primeiro lugar, e que eu possa me sentir realizado cada vez que conseguir aprender algo novo, e que eu possa me sentir orgulhoso de mim mesmo por estar investindo tempo e esforço em algo tão valioso como o conhecimento em JavaScript, e que eu possa me sentir motivado a continuar aprendendo cada vez mais sobre essa linguagem incrível, e que eu possa me sentir inspirado por outras pessoas que também estão aprendendo JavaScript ou já são desenvolvedores experientes nessa linguagem, e que eu possa me sentir parte de uma comunidade incrível de pessoas apaixonadas por JavaScript, e que eu possa me sentir grato por ter acesso a tantos recursos incríveis para aprender JavaScript hoje em dia, como cursos online, tutoriais em vídeo, livros digitais, fóruns de discussão, grupos de estudo, eventos presenciais ou virtuais, entre outros),')
+  if (true) { 
+    console.log ('estou muito feliz de vdd ')
+
+  } else {
+    console.log ('estou muito triste de vdd ')
+    
+
+    }
+
+    
+    for (let i = 0; i < 2; i++) {
+      console.log ('estou muito feliz de vdd ')
+    }
